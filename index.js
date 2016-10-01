268cd0f0207bb6d9e6929f85035f05251cdce2c6
Rama Krishna <siddarth21@hotmail.com>
2016-10-02 01:13:05 +0800

    remove shutdown functionality

--- index.js
+++ index.js
@@ -142,32 +142,32 @@ controller.hears(['what is my name', 'who am i'], 'direct_message,direct_mention
 });
 
 
-controller.hears(['shutdown'], 'direct_message,direct_mention,mention', function(bot, message) {
-
-    bot.startConversation(message, function(err, convo) {
-
-        convo.ask('Are you sure you want me to shutdown?', [
-            {
-                pattern: bot.utterances.yes,
-                callback: function(response, convo) {
-                    convo.say('Bye!');
-                    convo.next();
-                    setTimeout(function() {
-                        process.exit();
-                    }, 3000);
-                }
-            },
-        {
-            pattern: bot.utterances.no,
-            default: true,
-            callback: function(response, convo) {
-                convo.say('*Phew!*');
-                convo.next();
-            }
-        }
-        ]);
-    });
-});
+// controller.hears(['shutdown'], 'direct_message,direct_mention,mention', function(bot, message) {
+//
+//     bot.startConversation(message, function(err, convo) {
+//
+//         convo.ask('Are you sure you want me to shutdown?', [
+//             {
+//                 pattern: bot.utterances.yes,
+//                 callback: function(response, convo) {
+//                     convo.say('Bye!');
+//                     convo.next();
+//                     setTimeout(function() {
+//                         process.exit();
+//                     }, 3000);
+//                 }
+//             },
+//         {
+//             pattern: bot.utterances.no,
+//             default: true,
+//             callback: function(response, convo) {
+//                 convo.say('*Phew!*');
+//                 convo.next();
+//             }
+//         }
+//         ]);
+//     });
+// });
 
 controller.hears(['volunteer', 'kind soul'], 'direct_message,direct_mention,mention', function(bot, message) {
   var volunteer = students[Math.floor(Math.random()*students.length)];
